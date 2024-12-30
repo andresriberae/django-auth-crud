bd366ae5ab64e18b1c14e786ba898b0ad91db15a
andresriberae <kcandres21@gmail.com>
2024-12-29 23:43:34 -0400

    agregando tailwind al build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+
+python manage.py tailwind install
+python manage.py tailwind build
+python manage.py collectstatic --no-input
